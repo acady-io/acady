f54771c697ee56bbfe80104790cccb1e40f56639
Christian Schab <christian.schab@wusoma.com>
2022-01-10 17:35:56 +0100

    new: Remove Netlify support

--- src/connectors/netlify-connector.ts
+++ src/connectors/netlify-connector.ts
@@ -1,17 +1,19 @@
 import {NetlifyCredentials} from "../dto/credentials/netlify-credentials";
 
-import {NetlifyAPI} from 'netlify';
+//import {NetlifyAPI} from 'netlify';
 
 export class NetlifyConnector {
 
     static async listSites(credentials: NetlifyCredentials) {
-        const client = new NetlifyAPI(credentials.apiToken);
-        return await client.listSites();
+        // const client = new NetlifyAPI(credentials.apiToken);
+        // return await client.listSites();
+        return [];
     }
 
-    static async getCurrentUser(crdentials: NetlifyCredentials) {
-        const client = new NetlifyAPI(crdentials.apiToken);
-        return await client.getCurrentUser();
+    static async getCurrentUser(crdentials: NetlifyCredentials): Promise<any> {
+        // const client = new NetlifyAPI(crdentials.apiToken);
+        // return await client.getCurrentUser();
+        return {};
     }
 
 
